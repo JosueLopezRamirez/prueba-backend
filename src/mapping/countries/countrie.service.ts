90db5d703af81578821f046e13f6fab1690c58f6
JosueLopezRamirez <josue.ismael.lopez.ramirez@gmail.com>
2019-09-25 09:30:30 -0600

    Quitando CRUD de la tabla countries

--- src/mapping/countries/countrie.service.ts
+++ src/mapping/countries/countrie.service.ts
@@ -9,11 +9,6 @@ export class CountrieService {
 
     constructor(@InjectRepository(Countrie) private countrieRepository: Repository<Countrie>) { }
 
-    //Get a all users
-    // async getAll(): Promise<Countrie[]> {
-    //     return await this.countrieRepository.find();
-    // }
-
     //Obtener unicamente los nombres y los codigos de telefono de los paises
     async getAllCountries(): Promise<any>{
         let result = await this.countrieRepository.find({
@@ -27,31 +22,4 @@ export class CountrieService {
         return { data: { error: { message: 'Bad request', status: 200, ok: false } } }
     }
 
-    // //Find user by id
-    // async findById(id: number): Promise<any> {
-    //     let result = await this.countrieRepository.findOne({
-    //         where: {
-    //             id: id
-    //         }
-    //     });
-    //     if(result!==undefined){
-    //         return {data: { message: 'Petition successfuly', ok:true, status:200, data: result } }
-    //     }
-    //     return { data: { error: { message: 'Bad request', status: 200, ok: false } } }
-    // }
-
-    // //Create a new user
-    // async create(countrie: countrieDto): Promise<Countrie>{
-    //     return this.countrieRepository.save(countrie);
-    // }
-
-    // //Update a user
-    // async update(countrie: countrieDto): Promise<Countrie> {
-    //     return this.countrieRepository.save(countrie);
-    // }
-
-    // //Delete a user
-    // async delete(countrie: Countrie) {
-    //     return this.countrieRepository.delete(countrie);
-    // }
 }
