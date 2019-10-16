3bd879540fabfb22954d4c7de1d2a65f4ec94ab0
pmeneses <pedro200994@outlook.com>
2019-10-16 10:18:43 -0600

    agregamos indicador para cada estado de la orden

--- src/mapping/skiper-orders-status/skiper-orders-status.dto.ts
+++ src/mapping/skiper-orders-status/skiper-orders-status.dto.ts
@@ -5,11 +5,13 @@ import { SkiperOrderTracing } from "../skiper-order-tracing/skiper-order-tracing
 export class SkiperOrdersStatusInput {
     id: number;
     name: string;
+    indicador: string
 }
 
 @ObjectType()
 export class SkiperOrdersStatusDto {
     id: number;
     name: string;
+    indicador: string;
     SkiperOrderTracing: SkiperOrderTracing[];
 }
