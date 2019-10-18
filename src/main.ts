1732d64251f5958e9e5e3345089f87f71fdc14f0
pmeneses <pedro200994@outlook.com>
2019-10-18 09:04:49 -0600

    aceptando el header autorization

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
     next();
   });
 
