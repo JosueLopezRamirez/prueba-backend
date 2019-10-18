f1691ec5949dd96f45b4299cee64e1ca112dc0ea
pmeneses <pedro200994@outlook.com>
2019-10-18 08:48:03 -0600

    Se agregan las cords al app

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,13 @@ async function bootstrap() {
   //Add Compression
   app.use(compression());
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   await app.listen(process.env.PORT || 4000);
 
 }
