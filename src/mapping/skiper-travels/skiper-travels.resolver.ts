9e17bed851ccad2b4dedb84e535ac20114ed6c82
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2019-10-25 09:23:29 -0600

    subo mapeo skiper travel

--- src/mapping/skiper-travels/skiper-travels.resolver.ts
+++ src/mapping/skiper-travels/skiper-travels.resolver.ts
@@ -4,7 +4,7 @@ import { SkiperTravelsService } from './skiper-travels.service';
 @Resolver('SkiperTravels')
 export class SkiperTravelsResolver{
     constructor(private readonly service: SkiperTravelsService) { }
-
+    // por ahora esto nada mas
     @Query()
     async getAllSkiperTravels() {
         return await this.service.getAll();
